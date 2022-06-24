9a7f8a159e16edbf4448cc840b9f358cf2688f95
birr0 <brianrogers0401@gmail.com>
2022-06-24 14:46:08 +0000

    ccd_visit function

--- PlottingTools/Source/observations.py
+++ PlottingTools/Source/observations.py
@@ -10,8 +10,7 @@ import numpy as np
 from DataAccessLayer import create_connection_and_queryDB
 from matplotlib import pyplot as plt
 from plots.scatter import ScatterPlot
-from plots.symbols import degree
-import seaborn as sns
+from plots.symbols import degree, beta, _lambda
 
 
 def object_detections(start: float, end: float):
@@ -30,6 +29,25 @@ def object_detections(start: float, end: float):
 
 
     return ScatterPlot(data = df, x = "ra", y = "decl", xlabel=f"R.A. ({degree})", ylabel=f"Dec ({degree})", title=f"Observations during the period {start}-{end}")
+
+def ccd_visit(ccd_visit_id : int):
+    query = """
+    SELECT
+        eclipticLambda as lon, eclipticBeta as lat, ccdVisitId, midPointTAI
+    FROM
+        diaSources
+        JOIN ssSources USING(diaSourceId)
+    WHERE
+        ccdVisitId = %(ccd_visit_id)s
+    """
+    
+    df = create_connection_and_queryDB(query, dict(ccd_visit_id = ccd_visit_id))
+    
+    return ScatterPlot(data = df, x = "lat", y = "lon", xlabel=f"Eccliptic {beta} ({degree})", ylabel=f"Eccliptic {_lambda} ({degree})", title=f"CCD visit id {ccd_visit_id}")
+    
+    
+    
+    
 
 
 
