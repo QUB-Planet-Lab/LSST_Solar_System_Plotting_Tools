4ca4dc055bd356a7baee131749091836cb9d7f3e
birr0 <brianrogers0401@gmail.com>
2022-06-27 10:09:27 +0000

    Increased Plot customization

--- PlottingTools/Source/plots/plot.py
+++ PlottingTools/Source/plots/plot.py
@@ -1,22 +1,33 @@
 from typing import Literal
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 class Plot():
     '''A parent class which all plotting classes inherit from.'''
-    def __init__(self, data, xlabel : str, ylabel : str = "", title: str = ""):     
+    def __init__(self, data, xlabel : str, ylabel : str = "", title: str = "", rc_params = {}):     
         self.data = data
+        if rc_params:
+            self.context = sns.set(rc=rc_params)# add all rc params here. Can be used for seaborn and matplotlib as long as it is figure level.
+        else:
+            self.context = None
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
-       
-    def save(self, fig_name : str, extension : Literal['png', 'jpeg', 'pdf'] = 'png'):
-        self.plot
+        
+        
+    def add_plot_info(self):
         plt.title(self.title)
         if self.xlabel:
             plt.xlabel(self.xlabel)
         if self.ylabel:
             plt.ylabel(self.ylabel)
-        plt.savefig(f"{fig_name}.{extension}")
+        # add legend here also
+        
+    
+    def save(self, file_name : str, extension : Literal['png', 'jpeg', 'pdf'] = 'png'):
+        # Need to validate these arguments, filepath?
+        self.plot
+        self.add_plot_info()
+        plt.savefig(f"{file_name}.{extension}")
         
 
