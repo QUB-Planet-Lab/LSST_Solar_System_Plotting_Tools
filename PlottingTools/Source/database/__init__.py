fe5d184bd8eaa6e5dfd4fdfa7915fe0238913388
birr0 <brianrogers0401@gmail.com>
2022-06-30 09:01:39 +0000

    Fix unit test

--- PlottingTools/Source/database/__init__.py
+++ PlottingTools/Source/database/__init__.py
@@ -5,11 +5,15 @@ database module deals with connections and transactions to the the database.
 from .db_config import DBConfig
 from .db import DB
 
+
+
 try:
     with open("/home/shared/sssc-db-pass.txt") as pwf:
         pwd = pwf.read()
 
-
+except:
+    print("Error accessing the pwd file")
+try:
     db_url = DBConfig(password = pwd,
                       user = 'sssc',
                       port = '5432',
@@ -18,10 +22,13 @@ try:
                       driver ='psycopg2',
                       host = 'epyc.astro.washington.edu'
                      ).database_url
-
     db = DB(db_url = db_url)
-
+    
 except:
-    raise Exception("PWD file not found.")
+    db = None
+    print("Cannot create db_url")
+    
+
+
 
     
