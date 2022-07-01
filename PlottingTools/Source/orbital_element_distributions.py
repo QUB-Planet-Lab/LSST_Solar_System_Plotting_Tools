2e7e149c4afbec99fff23368053e92dd9a59a6fa
birr0 <brianrogers0401@gmail.com>
2022-07-01 14:27:14 +0000

    Refactor orbital element code, addded semi-major-axis distribution functions

--- PlottingTools/Source/orbital_element_distributions.py
+++ PlottingTools/Source/orbital_element_distributions.py
@@ -1,31 +1,42 @@
-from DataAccessLayer import create_connection_and_queryDB
 from plots.box import BoxPlot, BoxenPlot
 from plots.violin import ViolinPlot
 from plots.symbols import DEGREE
 
+from database import db
+from database.schemas import DIASource, diasource, mpcorb
+from database.validators import validate_times, validate_filters,\
+    validate_perihelion, validate_inclination, validate_semi_major_axis
+from database.format_time import format_times
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from database import db
-from sqlalchemy import select, func
-from sqlalchemy import func
-from database.schemas import DIASource, diasource, mpcorb
-from database.validators import validate_times, validate_filters, validate_perihelion, validate_inclination
-
 from typing import Optional, Literal
+
+from sqlalchemy import select, func
 
 
 PLOT_TYPES = ['BOX', 'BOXEN', 'VIOLIN']
 
-def eccentricity(filters: Optional[list] = None,
-                 min_e: float = 0, max_e : float = None,
-                 start_time : Optional[float] = None, end_time : Optional[float] = None,
-                 plot_type: Literal[PLOT_TYPES] = 'BOX'):
-    
-    """ eccentricity() returns a BoxenPlot object
-    
-    """
+ELEMENTS = {'e' : {'label': 'Eccentricity','unit' : None},\
+            'a' : {'label': 'Semi-Major axis', 'unit': 'au'},\
+            'peri' : {'label': 'Perihelion', 'unit': 'au'}, \
+            'incl' : {'label' : 'Inclination', 'unit' : DEGREE}
+           }
 
+def base(
+         stmt,
+         element, # e, incl, a, peri
+         filters: Optional[list] = None,
+         min_e: float = 0, max_e : float = None,
+         min_peri: float = 0.0, max_peri : float = None,
+         min_a: float = 0.0, max_a : float = None,
+         min_incl: float = 0.0, max_incl: float = None,
+         start_time : Optional[float] = None, end_time : Optional[float] = None,
+         plot_type: Literal[PLOT_TYPES] = 'BOX',
+         title : Optional[str] = None,
+        ):
+    
     plot_type = plot_type.upper()
 
     if plot_type not in PLOT_TYPES:
@@ -44,201 +55,222 @@ def eccentricity(filters: Optional[list] = None,
     
     if filters:
         filters = validate_filters(list(set(filters)))
-      
         conditions.append(diasource.c['filter'].in_(filters))
-    print(filters)
-    df = db.query(
-        select( mpcorb.c['e'], diasource.c['filter']).join(
-            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid']).where(
-                *conditions
-        ).distinct() 
-        .limit(1000)
-    )
-    if df.empty:
-        raise Exception(f"""No results returned for your query: 
-                    
-                    filters: {filters},
-                    start_time: {start_time},
-                    end_time: {end_time},
-                    min_e: {min_e},
-                    max_peri: {max_e},
-                """)
-    
-    if filters:
-        filters_str = ", ".join(filters) if len(filters) > 1 else filters[0]
         
-        args = dict(data = df, x="e", y="filter", title=f"Eccentricity distributions across filters ({filters_str}) from {start_time} - {end_time}", xlabel="Perihelion (au)", ylabel="Filters",rc_params={'figure.figsize':(12,8)})
-        
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
-        else:
-            return BoxenPlot(**args)
-    
-    else:
-        args = dict(data = df, x = "e", xlabel = "Eccentricity", title=f"Eccentricity distributions across all filters {start_time}-{end_time}", rc_params={'figure.figsize':(12,8)})
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
-        else:
-            return BoxenPlot(**args)    
-    
-def perihelion(filters: Optional[list] = None,
-               min_peri: float = 0, max_peri : float = None,
-               start_time : Optional[float] = None, end_time : Optional[float] = None,
-               plot_type: Literal[PLOT_TYPES] = 'BOX'):
-    
-    plot_type = plot_type.upper()
-
-        
-    if plot_type not in PLOT_TYPES:
-        raise TypeError(f"{plot_type} is not a valid chart option. Valid plot options include 'BOX', 'BOXEN' or 'VIOLIN'")
-    
-    conditions = []
-    
     min_peri, max_peri = validate_perihelion(min_peri, max_peri)
+    min_incl, max_incl = validate_inclination(min_incl = min_incl, max_incl = max_incl)
+    min_a, max_a = validate_semi_major_axis(min_a, max_a)
+    
     
     if max_peri:
-        conditions.append(mpcorb.c['peri'] < max_peri)
+        conditions.append(mpcorb.c['peri'] < min_peri)
     if min_peri:
-        conditions.append(mpcorb.c['peri'] > min_peri)
+        conditions.append(mpcorb.c['peri'] > max_peri)
     
-    start_time, end_time = validate_times(start_time = start_time, end_time = end_time)
+    if min_e:
+        condition.append(mpcorb.c['e'] >= min_e)
     
-    if start_time:
-        conditions.append(diasource.c['midpointtai'] >= start_time)
-    
-    if end_time:
-        conditions.append(diasource.c['midpointtai'] <= end_time)
-    
-    if filters:
-        filters = validate_filters(list(set(filters)))
-      
-        conditions.append(diasource.c['filter'].in_(filters))
-    
-    df = db.query(
-        select( mpcorb.c['peri'], diasource.c['filter']).join(
-            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid']).where(
-                *conditions
-        ).distinct()
-        .limit(10000)
-    )
-    
-    if df.empty:
-        raise Exception(f"""No results returned for your query: 
-                    
-                    filters: {filters},
-                    start_time: {start_time},
-                    end_time: {end_time},
-                    min_peri: {min_peri},
-                    max_peri: {max_peri},
-                    
-                """)
+    if max_e:
+        condition.append(mpcorb.c['e'] <= max_e)
         
-        
-    if filters:
-        filters_str = ", ".join(filters) if len(filters) > 1 else filters[0]
-        
-        args = dict(data = df, x="peri", y="filter", title=f"Perihelion distributions across filters ({filters_str}) from {start_time} - {end_time}", xlabel="Perihelion (au)", ylabel="Filters",rc_params={'figure.figsize':(12,8)})
-        
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
-        else:
-            return BoxenPlot(**args)
-    
-    else:
-        args = dict(data = df, x = "peri", xlabel = "Perihelion (au)", title=f"Perihelion distributions across all filters {start_time}-{end_time}", rc_params={'figure.figsize':(12,8)})
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
-        else:
-            return BoxenPlot(**args)
-
-def calculate_sm_axis(peri, e):
-    return peri * (1 - e)
-
-def semi_major_axis():
-    #todo
-    return
-
-
-def inclination(filters: Optional[list] = None,
-               min_incl: float = 0, max_incl : float = None,
-               start_time : Optional[float] = None, end_time : Optional[float] = None,
-               plot_type: Literal[PLOT_TYPES] = 'BOX'):
-    
-    plot_type = plot_type.upper()
-
-    if plot_type not in PLOT_TYPES:
-        raise TypeError(f"{plot_type} is not a valid chart option. Valid plot options include 'BOX', 'BOXEN' or 'VIOLIN'")
-    
-    conditions = []
-    
-    if filters:
-        filters = validate_filters(list(set(filters)))
-      
-        conditions.append(diasource.c['filter'].in_(filters))
-        
-    start_time, end_time = validate_times(start_time = start_time, end_time = end_time)
-    min_incl, max_incl = validate_inclination(min_incl = min_incl, max_incl = max_incl)
-    
-    if start_time:
-        conditions.append(diasource.c['midpointtai'] >= start_time)
-    
-    if end_time:
-        conditions.append(diasource.c['midpointtai'] <= end_time)
-        
-    
     if min_incl:
         conditions.append(mpcorb.c['incl'] >= min_incl)
     
     if max_incl:
         conditions.append(mpcorb.c['incl'] <= max_incl)
     
-
-    df = db.query(
-        select(mpcorb.c['incl'], diasource.c['filter']).join(
-            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid']).where(
-                *conditions
-        ).distinct().limit(10)
-    )
+    if min_a:
+        conditions.append((mpcorb.c['peri'] / (1 - mpcorb.c['e']) ) >= min_a)
     
+    if max_a:
+        conditions.append((mpcorb.c['peri'] / (1 - mpcorb.c['e']) ) <= max_a)
+        
+        
+    df = db.query(
+        stmt.where(
+            *conditions
+        ) 
+   )
     if df.empty:
-        raise Exception(f"""No results returned for your query: 
-                    
-                    filters: {filters},
-                    start_time: {start_time},
-                    end_time: {end_time},
-                    min_incl: {min_incl},
-                    max_incl: {max_incl},
-                    
-                """)
+        query = f"""No results returned for your query:\n"""
+        if filters:
+            query.append(f"filters : {filters}\n")
+        if start_time:
+            query.append(f"start_time : {start_time}\n")
+        if end_time:
+            query.append(f"end_time : {end_time}\n")
+        if min_e:
+            query.append(f"min_e : {min_e}\n")
+        if max_e:
+            query.append(f"max_e : {max_e}\n")
+        if min_a:
+            query.append(f"min_a : {min_a}\n")
+        if max_a:
+            query.append(f"max_a : {max_a}\n")
+        if min_incl:
+            query.append(f"min_incl : {min_incl}\n")
+        if max_incl:
+            query.append(f"max_incl : {max_incl}\n")
+        if min_peri:
+            query.append(f"min_peri : {min_peri}\n")
+        if max_peri:
+            query.append(f"max_peri : {max_peri}\n")
         
-        
+        query = query[0:-1]
+                         
+        raise Exception(query)
+
+    '''
+    # TODO add plot info card
+    if start_time:
+        plot_info['start'] = start_time
+    
+    if end_time:
+        plot_info['end'] = end_time
+    
+    if min_a:
+        plot_info['min_a'] = min_a
+    
+    '''
+    start_time, end_time = format_times([start_time, end_time], _format="ISO")
+    label = ELEMENTS[element]['label']
+    unit = ELEMENTS[element]['unit']
+    args = dict(data = df, x = element, 
+                xlabel = f'{label} ({unit})' , 
+                rc_params={'figure.figsize':(12,8),
+                           "figure.titlesize" : 18, 
+                           "axes.labelsize" : 16,
+                           #"axes.facecolor": "#0000ff",
+                           "figure.edgecolor": "black",
+                           "patch.facecolor": "#0000ff",
+                          }
+               ) # plot_info = plot_info
+    
     if filters:
+        args['y'] = "filter"
+        args['ylabel'] = "Filter"
         filters_str = ", ".join(filters) if len(filters) > 1 else filters[0]
         
-        args = dict(data = df, x="incl", y="filter", title=f"Inclination distributions across filters ({filters_str}) from {start_time} - {end_time}", xlabel=f"Inclination ({DEGREE})", ylabel="Filters",rc_params={'figure.figsize':(12,8)})
-        
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
+        if title:
+            args['title']= title
         else:
-            return BoxenPlot(**args)
-    
+            args['title'] =f"{label} distributions across filters ({filters_str})\n {start_time} - {end_time}"
     else:
-        args = dict(data = df, x = "incl", xlabel = f"Inclination ({DEGREE})", title=f"Inclination distributions across all filters {start_time}-{end_time}", rc_params={'figure.figsize':(12,8)})
-        if plot_type == "BOX":
-            return BoxPlot(**args)
-        elif plot_type == "VIOLIN":
-            return ViolinPlot(**args)
+        
+        if title:
+            args['title']= title 
         else:
-            return BoxenPlot(**args)
+            args['title'] = f"{label} distributions across all filters\n {start_time}-{end_time}"    
+    
+    if plot_type == "BOX":
+        return BoxPlot(**args)
+    elif plot_type == "VIOLIN":
+        return ViolinPlot(**args)
+    else:
+        return BoxenPlot(**args)
+    
+def eccentricity(filters: Optional[list] = None,
+                 min_e: float = None, max_e : float = None,
+                 min_peri: float = 0.0, max_peri : float = None,
+                 min_a: float = 0.0, max_a : float = None,
+                 min_incl: float = 0.0, max_incl: float = None,
+                 start_time : Optional[float] = None, end_time : Optional[float] = None,
+                 plot_type: Literal[PLOT_TYPES] = 'BOX',
+                 title : Optional[str] = None
+                ):
+    
+    """   
+    """
+    
+    return base(
+        filters = filters,
+        start_time = start_time, end_time = end_time,
+        min_e = min_e, max_e = max_e,
+        min_a = min_a, max_a = max_a,
+        min_peri = min_peri, max_peri = max_peri,
+        min_incl = min_incl, max_incl = max_incl,
+        plot_type = plot_type,
+        title = title,
+        element = 'e',
+        stmt = select(
+            func.distinct(mpcorb.c['ssobjectid']), mpcorb.c['e'], diasource.c['filter']).join(
+            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid'])
+    )
         
+    
+def perihelion(filters: Optional[list] = None,
+                 min_e: float = 0, max_e : float = None,
+                 min_peri: float = 0.0, max_peri : float = None,
+                 min_a: float = 0.0, max_a : float = None,
+                 min_incl: float = 0.0, max_incl: float = None,
+                 start_time : Optional[float] = None, end_time : Optional[float] = None,
+                 plot_type: Literal[PLOT_TYPES] = 'BOX',
+                 title : Optional[str] = None,
+                  ):
+    
+    return base(
+        filters = filters,
+        start_time = start_time, end_time = end_time,
+        min_e = min_e, max_e = max_e,
+        min_a = min_a, max_a = max_a,
+        min_peri = min_peri, max_peri = max_peri,
+        min_incl = min_incl, max_incl = max_incl,
+        plot_type = plot_type,
+        title = title,
+        element = 'peri',
+        stmt = select(func.distinct(mpcorb.c['ssobjectid']), mpcorb.c['peri'], diasource.c['filter']).join(
+            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid'])
+    )
         
+    
+def inclination(filters: Optional[list] = None,
+                 min_e: float = 0, max_e : float = None,
+                 min_peri: float = 0.0, max_peri : float = None,
+                 min_a: float = 0.0, max_a : float = None,
+                 min_incl: float = 0.0, max_incl: float = None,
+                 start_time : Optional[float] = None, end_time : Optional[float] = None,
+                 plot_type: Literal[PLOT_TYPES] = 'BOX',
+                 title : Optional[str] = None):
+    
+    
+    return base(
+        filters = filters,
+        start_time = start_time, end_time = end_time,
+        min_e = min_e, max_e = max_e,
+        min_a = min_a, max_a = max_a,
+        min_peri = min_peri, max_peri = max_peri,
+        min_incl = min_incl, max_incl = max_incl,
+        plot_type = plot_type,
+        title = title,
+        element = 'incl',
+        stmt = select(func.distinct(mpcorb.c['ssobjectid']),mpcorb.c['incl'], diasource.c['filter']).join(
+            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid'])
+    )
+
+    
+        
+def semi_major_axis(filters: Optional[list] = None,
+                 min_e: float = None, max_e : float = None,
+                 min_peri: float = 0.0, max_peri : float = None,
+                 min_a: float = 0.0, max_a : float = None,
+                 min_incl: float = 0.0, max_incl: float = None,
+                 start_time : Optional[float] = None, end_time : Optional[float] = None,
+                 plot_type: Literal[PLOT_TYPES] = 'BOX',
+                 title : Optional[str] = None
+                ):
+    
+    return base(
+        filters = filters,
+        start_time = start_time, end_time = end_time,
+        min_e = min_e, max_e = max_e,
+        min_a = min_a, max_a = max_a,
+        min_peri = min_peri, max_peri = max_peri,
+        min_incl = min_incl, max_incl = max_incl,
+        plot_type = plot_type,
+        title = title,
+        element = 'a',
+        stmt = select(func.distinct(mpcorb.c['ssobjectid']).label('ssobjectid'), mpcorb.c['peri'], (mpcorb.c['peri'] / (1 - mpcorb.c['e'])).label('a') , diasource.c['filter']).join(
+            diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid'])
+    )
+
+    
