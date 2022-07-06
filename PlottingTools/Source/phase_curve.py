d8a9069c8056793bd9e2d671b9ccf9a6e913c79a
birr0 <brianrogers0401@gmail.com>
2022-07-06 13:05:17 +0000

    Reduced phase curve with fit

--- PlottingTools/Source/phase_curve.py
+++ PlottingTools/Source/phase_curve.py
@@ -11,6 +11,10 @@ from plots.symbols import DEGREE
 from plots.styles.filter_color_scheme import COLOR_SCHEME
 
 import pandas as pd
+import numpy as np
+
+from sbpy.photometry import HG
+
 
 
 def phase_curve(filters: Optional[list] = None,
@@ -24,7 +28,8 @@ def phase_curve(filters: Optional[list] = None,
     start_time, end_time = validate_times(start_time = start_time, end_time = end_time)
 
 
-    cols = [diasource.c['magsigma'], diasource.c['filter'], mpcorb.c['mpcdesignation'], diasource.c['ssobjectid'], diasource.c['midpointtai'],diasource.c['mag'], sssource.c['phaseangle']]
+    cols = [diasource.c['magsigma'], diasource.c['filter'], mpcorb.c['mpcdesignation'], diasource.c['ssobjectid'], diasource.c['midpointtai'],diasource.c['mag'], sssource.c['phaseangle'], sssource.c['topocentricdist'], sssource.c['heliocentricdist']]
+    
     
     conditions = []
     
@@ -32,8 +37,13 @@ def phase_curve(filters: Optional[list] = None,
         
         filters = validate_filters(list(set(filters)))
         conditions.append(diasource.c['filter'].in_(filters))
-           
+        
         #conditions.append(diasource.c['filter'] == filter)
+        
+        for _filter in filters:
+            calc_mags = [ssobjects.c[f'{_filter}h'], ssobjects.c[f'{_filter}g12'], ssobjects.c[f'{_filter}herr'], ssobjects.c[f'{_filter}g12err'], ssobjects.c[f'{_filter}chi2']]
+            for item in calc_mags:              
+                cols.append(item)
         
     if mpcdesignation:
         conditions.append(mpcorb.c['mpcdesignation'] == mpcdesignation)
@@ -47,7 +57,7 @@ def phase_curve(filters: Optional[list] = None,
     if end_time:
         conditions.append(diasource.c['midpointtai'] <= end_time)
     
-    stmt = select(*cols).join(diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid']).join(sssource, sssource.c['diasourceid'] == diasource.c['diasourceid']).where(*conditions)
+    stmt = select(*cols).join(diasource, diasource.c['ssobjectid'] == mpcorb.c['ssobjectid']).join(ssobjects, ssobjects.c['ssobjectid'] == mpcorb.c['ssobjectid']).join(sssource, sssource.c['diasourceid'] == diasource.c['diasourceid']).where(*conditions)
     
     df = db.query(
         stmt
@@ -71,22 +81,32 @@ def phase_curve(filters: Optional[list] = None,
         print(query)
         return
     
+    
+    
+    df["cmag"] = df["mag"] - 5*np.log10(df["topocentricdist"]*df["heliocentricdist"])
+    
+    
+    
     if filters:
-        pc = ScatterPlot(data = pd.DataFrame(columns = df.columns.values), x = "phaseangle", y="mag", title=title if title else f"Phase curve for {mpcdesignation if mpcdesignation else ssobjectid}\n {start_time} - {end_time}", xlabel=f"Phase Angle ({DEGREE})", ylabel="Magnitude")
-
+        #note is yerr = 'magsigma'?
+        pc = ScatterPlot(data = pd.DataFrame(columns = df.columns.values), x = "phaseangle", y="mag", title=title if title else f"Phase curve for {mpcdesignation if mpcdesignation else ssobjectid}\n", xlabel=f"Phase Angle ({DEGREE})", ylabel="Reduced magnitude")
+        
+                         
+                         
         for _filter in filters:
             df_filter = df[df['filter'] == _filter]
+            _ph = sorted(df_filter["phaseangle"])
+            _mag = HG.evaluate(np.deg2rad(_ph), df_filter[f'{_filter}h'], df_filter[f'{_filter}g12err'])
+                         
             if not df_filter.empty:
-                pc.ax.errorbar(data = df_filter , x = "phaseangle", y = "mag", yerr=df_filter['magsigma'], label=_filter, fmt='o', c = COLOR_SCHEME[_filter])
-        # add filter to plot
+                pc.ax.errorbar(data = df_filter , x = "phaseangle", y = "cmag", yerr=df_filter['magsigma'], label=_filter, fmt='o', c = COLOR_SCHEME[_filter])
+                pc.ax.plot(_ph, _mag, c = COLOR_SCHEME[_filter])               
         pc.ax.legend(loc="upper right")        
 
     else:
-        pc = ScatterPlot(data = df, x = "phaseangle", y="mag", yerr=df["magsigma"], title=title if title else f"Phase curve for {mpcdesignation if mpcdesignation else ssobjectid}\n {start_time} - {end_time}", xlabel=f"Phase Angle ({DEGREE})", ylabel="Magnitude")
+        pc = ScatterPlot(data = df, x = "phaseangle", y="cmag", yerr=df["magsigma"], title=title if title else f"Phase curve for {mpcdesignation if mpcdesignation else ssobjectid}\n", xlabel=f"Phase Angle ({DEGREE})", ylabel="Reduced Magnitude")
 
-        
     pc.ax.invert_yaxis()
     
     return pc
-
- 
+    
