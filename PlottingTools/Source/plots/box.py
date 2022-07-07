2b1f5ec3aecd443014803da09363e8e1caa5942e
birr0 <brianrogers0401@gmail.com>
2022-07-07 10:33:22 +0000

    Rewrite of box and violin plots to matplotlib. Color co-ordination for filters added. Specific functionality needs moved later

--- PlottingTools/Source/plots/box.py
+++ PlottingTools/Source/plots/box.py
@@ -1,8 +1,12 @@
 from .plot import Plot
-from seaborn import boxplot, boxenplot
+from seaborn import boxenplot
 import matplotlib.pyplot as plt
 
+
+from .styles.filter_color_scheme import COLOR_SCHEME
+
 class BoxPlot(Plot):
+    # does specific functionality need to be removed from here?
     def __init__(self, data, x = None, y = None, xlabel: str = "" , ylabel : str = "", title: str = "", rc_params : dict = {}, ax = None):
         super().__init__(data, xlabel, ylabel, title, rc_params)
 
@@ -12,12 +16,33 @@ class BoxPlot(Plot):
         
         
         if not y:
-            self.plot = boxplot(x = data[self.x], ax = self.ax).set(xlabel="", ylabel="")
+            self.plot = self.ax.boxplot(x = data[self.x], labels=[''], vert=False, patch_artist = True)
+            for median in self.plot['medians']:
+                median.set_color('black')
+                
+                for patch in self.plot['boxes']:
+                    patch.set(facecolor="#3CAE3F") # add color optionality
+                
         elif not x:
-            self.plot = boxplot(y = data[self.y], ax = self.ax).set(xlabel="", ylabel="")
+            self.plot = self.ax.boxplot(x = data[self.y], vert=True, patch_artist=True, labels=[''])
+            for median in self.plot['medians']:
+                median.set_color('black')
+            for patch in self.plot['boxes']:
+                patch.set(facecolor="#3CAE3F")
+                
         else:
-            self.plot = boxplot(x = data[self.x], y = data[self.y], ax = self.ax).set(xlabel="", ylabel="")
+            # add optionality to have the boxes vertically aligned?
+            filter_x = []
+            cols = data[self.y].unique()
+            for col in cols:
+                filter_x.append(data[data[self.y] == col][self.x])
             
+            self.plot = self.ax.boxplot(x = filter_x, labels = cols,  vert=False, patch_artist=True)
+            for i, patch in enumerate(self.plot['boxes']):
+                patch.set(facecolor = COLOR_SCHEME[cols[i]])
+                
+            for median in self.plot['medians']:
+                median.set_color('black')
     
 class BoxenPlot(Plot):
     def __init__(self, data, x = None, y = None, xlabel: str = "" , ylabel : str = "", title: str = "", rc_params : dict = {}, ax = None):
--- PlottingTools/Source/plots/violin.py
+++ PlottingTools/Source/plots/violin.py
@@ -1,7 +1,12 @@
 from .plot import Plot
-from seaborn import violinplot
+import matplotlib.pyplot as plt
+
+from .styles.filter_color_scheme import COLOR_SCHEME
+
+import numpy as np 
 
 class ViolinPlot(Plot):
+    # does specific functionality need to be removed from here?
     def __init__(self, data, x = None, y = None, xlabel: str = "" , ylabel : str = "", title: str = "", rc_params : dict = {}, plot_info : dict = {}):
         super().__init__(data, xlabel, ylabel, title, rc_params, plot_info) 
         
@@ -9,8 +14,35 @@ class ViolinPlot(Plot):
         self.y = y
         
         if not y:
-            self.plot = violinplot(x = data[self.x], ax = self.ax).set(xlabel="", ylabel="")
+            self.plot = self.ax.violinplot(dataset = data[self.x], vert=False)
+            for pc in self.plot['bodies']:
+                pc.set_edgecolor("black") # add custom color here
+            self.ax.set_yticks([1], [''])
+            
         elif not x:
-            self.plot = violinplot(y = data[self.y], ax = self.ax).set(xlabel="", ylabel="")
+            self.plot = self.ax.violinplot(dataset = data[self.y], vert=True)
+            for pc in self.plot['bodies']:
+                pc.set_edgecolor("blue") # add custom color here
+            self.ax.set_xticks([1], [''])
+            
         else:
-            self.plot = violinplot(x = data[self.x], y = data[self.y], ax = self.ax).set(xlabel="", ylabel="")
+            
+            filter_x = []
+            cols = list(data[self.y].unique())
+            for col in cols:
+                filter_x.append(data[data[self.y] == col][self.x])
+                
+            
+            self.plot = self.ax.violinplot(dataset = filter_x, vert=False)
+            
+            self.ax.set_yticks(np.arange(1, len(cols) + 1), cols)
+            
+            for i, pc in enumerate(self.plot['bodies']):
+                pc.set_facecolor(COLOR_SCHEME[cols[i]])
+                pc.set_edgecolor('black')
+    
+        for partname in ('cbars','cmins','cmaxes'):
+            vp = self.plot[partname]
+            vp.set_edgecolor("black")
+            vp.set_linewidth(1)
+    
