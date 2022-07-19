83776dc10360d2789b9ec08a4bc085bb9ab9c5f9
birr0 <brianrogers0401@gmail.com>
2022-07-19 13:46:15 +0000

    Added create_orbit_conditions function to fix bug with forming db queries

--- PlottingTools/Source/objects_in_field.py
+++ PlottingTools/Source/objects_in_field.py
@@ -8,6 +8,9 @@ from database import db
 from database.schemas import diasource, mpcorb, ssobjects, sssource
 from database.validators import validate_times, validate_filters, validate_orbital_elements
 from database.format_time import format_times
+from database.conditions import create_orbit_conditions
+
+
 
 from plots import Plot, ScatterPlot
 from plots.styles.filter_color_scheme import COLOR_SCHEME
@@ -67,36 +70,7 @@ def objects_in_field(
         projection = projection.lower()
     
         
-    min_a, max_a, min_incl, max_incl, min_peri, max_peri, min_e, max_e = validate_orbital_elements(**orbital_elements)
-    
-    if min_peri:
-        conditions.append(mpcorb.c['peri'] >= min_peri)
-        
-    if max_peri:
-        conditions.append(mpcorb.c['peri'] <= max_peri)
-        
-    if min_incl:
-        conditions.append(mpcorb.c['incl'] >= min_incl)
-    
-    if max_incl:
-        conditions.append(mpcorb.c['incl'] <= max_incl)
-        
-    if min_a or max_a:
-        conditions.append(mpcorb.c['e'] > 0 )
-    
-        conditions.append(mpcorb.c['e'] < 1)
-    
-    elif not min_a and not max_a:
-        if min_e:
-            conditions.append(mpcorb.c['e'] >= min_e )
-        if max_e:
-            conditions.append(mpcorb.c['e'] <= min_e )
-                
-    if min_a:
-        conditions.append((mpcorb.c['peri'] / (1 - mpcorb.c['e']) ) >= min_a)
-    
-    if max_a:
-        conditions.append((mpcorb.c['peri'] / (1 - mpcorb.c['e']) ) <= max_a)
+    conditions = create_orbit_conditions(conditions = conditions, **orbital_elements)
         
     
     
