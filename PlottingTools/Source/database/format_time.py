1dcc5cf8cade2c26d1be9e48ef5767e0284b79ff
birr0 <brianrogers0401@gmail.com>
2022-07-19 11:22:45 +0000

    Remove dubious year warning from astropy

--- PlottingTools/Source/database/format_time.py
+++ PlottingTools/Source/database/format_time.py
@@ -1,27 +1,32 @@
 from astropy.time import Time
 from typing import Literal
+import warnings
 
 def format_times(times : list, _format : Literal["ISO", "MJD"] = "MJD"):
-    _format = _format.upper()
-    if _format == "ISO":
-        try:
-            return Time([str(time) for time in times], format="mjd").to_value('isot')
+    
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
         
-        except:
+        _format = _format.upper()
+        if _format == "ISO":
             try:
-                return Time(times, format="isot").to_value('isot')
+                return Time([str(time) for time in times], format="mjd").to_value('isot')
+
             except:
-                raise Exception("Invalid times used in input. Times must be input as MJD format")
-   
-    elif _format == "MJD":
-        try:
-            return Time(times, format='isot').to_value('mjd')
-        except:
+                try:
+                    return Time(times, format="isot").to_value('isot')
+                except:
+                    raise Exception("Invalid times used in input. Times must be input as MJD format")
+
+        elif _format == "MJD":
             try:
-                return Time(times, format='mjd').to_value('mjd')
+                return Time(times, format='isot').to_value('mjd')
             except:
-                print(times)
-                raise Exception("Invalid times used in input. Times must either be in ISO or MJD format")
-            
-    else:
-        raise Exception("format_time: The argument _format must be either ISO or MJD")
+                try:
+                    return Time(times, format='mjd').to_value('mjd')
+                except:
+                    print(times)
+                    raise Exception("Invalid times used in input. Times must either be in ISO or MJD format")
+
+        else:
+            raise Exception("format_time: The argument _format must be either ISO or MJD")
